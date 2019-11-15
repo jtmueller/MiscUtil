659dad3e1855d42d3f983b65c8fe18d6b572c326
Joel Mueller <joel.mueller@gmail.com>
2019-11-15 00:33:27 -0600

    Adding benchmarks for string-builder trim

---
+++ MiscUtil.Benchmarks/StringBuilderTrim.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Text;
+
+namespace MiscUtil.Benchmarks
+{
+    [Config(typeof(BenchmarkConfig))]
+    public class StringBuilderTrim
+    {
+        private static readonly char[] s_trimChars = new[] { ' ', '[', '^', '#', '?', '!' };
+        private const string s_str = "   [[[^^^###   [[[^^^### [[[  [[[^^^###   [[[^^^###   [[[^^^###   [[[^^^### [[[  [[[^^^###   [[[^^^###   [[[^^^###   [[[^^^### [[[  [[[^^^###   [[[^^^###   [[[^^^###   [[[^^^### [[[  [[[^^^###   [[[^^^###   [[[^^^###   [[[^^^###FROTZ [[[  [[[^^^###   [[[^^^###   [[[^^^###   [[[^^^###FROTZ [[[  [[[^^^###   [[[^^^###   [[[^^^###   [[[^^^### [[[  [[[^^^###   [[[^^^###   [[[^^^###   [[[^^^### [[[  [[[^^^###   [[[^^^###   [[[^^^###   [[[^^^### [[[  [[[^^^###   [[[^^^###   [[[^^^###   [[[^^^### [[[  [[[^^^###   [[[^^^###";
+        private StringBuilder _sb;
+
+        [Benchmark]
+        public void TrimEndChars()
+        {
+            _sb.TrimEnd(s_trimChars);
+        }
+
+        [Benchmark]
+        public void TrimStartChars()
+        {
+            _sb.TrimStart(s_trimChars);
+        }
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            _sb = new StringBuilder(s_str);
+        }
+    }
+}
