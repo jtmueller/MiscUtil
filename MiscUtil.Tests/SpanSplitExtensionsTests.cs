20c9450a14695bcf61a04c2eadd5a014b6809600
Joel Mueller <joel.mueller@gmail.com>
2020-06-11 01:39:08 -0500

    Updating span-splitting

--- MiscUtil.Tests/SpanSplitExtensionsTests.cs
+++ MiscUtil.Tests/SpanSplitExtensionsTests.cs
@@ -1,68 +1,252 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace MiscUtil.Tests
 {
     public class SpanSplitExtensionsTests
     {
-        [Theory]
-        [InlineData("", 'a')]
-        [InlineData("a", 'a')]
-        [InlineData("aaaa", 'a')]
-        [InlineData("abababab", 'a')]
-        [InlineData("babababa", 'a')]
-        [InlineData("aaaaab", 'a')]
-        [InlineData("baaaaa", 'a')]
-        [InlineData("zzzzzazzzzazzzzazzz", 'a')]
-        public void SplitOneChar(string str, char separator)
+        [Fact]
+        public static void SplitNoMatchSingleResult()
         {
-            var parts = new List<string>();
+            ReadOnlySpan<char> value = "a b".AsSpan();
 
-            var span = str.AsSpan();
-            foreach (var range in span.Split(separator))
-                parts.Add(span[range].ToString());
+            string expected = value.ToString();
+            var enumerator = value.Split(',');
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(expected, value[enumerator.Current].ToString());
+        }
 
-            Assert.Equal(str.Split(separator), parts);
+        [Fact]
+        public static void DefaultSpanSplitEnumeratorBehavior()
+        {
+            var charSpanEnumerator = new SpanSplitEnumerator<char>();
+            Assert.Equal(new Range(0, 0), charSpanEnumerator.Current);
+            Assert.False(charSpanEnumerator.MoveNext());
+
+            // Implicit DoesNotThrow assertion
+            charSpanEnumerator.GetEnumerator();
+        }
+
+        [Fact]
+        public static void ValidateArguments_OverloadWithoutSeparator()
+        {
+            ReadOnlySpan<char> buffer = default;
+
+            SpanSplitEnumerator<char> enumerator = buffer.Split();
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(new Range(0, 0), enumerator.Current);
+            Assert.False(enumerator.MoveNext());
+
+            buffer = ReadOnlySpan<char>.Empty;
+            enumerator = buffer.Split();
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(new Range(0, 0), enumerator.Current);
+            Assert.False(enumerator.MoveNext());
+
+            buffer = " ".AsSpan();
+            enumerator = buffer.Split();
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(new Range(0, 0), enumerator.Current);
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(new Range(1, 1), enumerator.Current);
+            Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public static void ValidateArguments_OverloadWithROSSeparator()
+        {
+            // Default buffer
+            ReadOnlySpan<char> buffer = default;
+
+            SpanSplitEnumerator<char> enumerator = buffer.Split(default(char));
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            enumerator = buffer.Split(' ');
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            // Empty buffer
+            buffer = ReadOnlySpan<char>.Empty;
+
+            enumerator = buffer.Split(default(char));
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            enumerator = buffer.Split(' ');
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            // Single whitespace buffer
+            buffer = " ".AsSpan();
+
+            enumerator = buffer.Split(default(char));
+            Assert.True(enumerator.MoveNext());
+            Assert.False(enumerator.MoveNext());
+
+            enumerator = buffer.Split(' ');
+            Assert.Equal(new Range(0, 0), enumerator.Current);
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(new Range(0, 0), enumerator.Current);
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(new Range(1, 1), enumerator.Current);
+            Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public static void ValidateArguments_OverloadWithStringSeparator()
+        {
+            // Default buffer
+            ReadOnlySpan<char> buffer = default;
+
+            SpanSplitEnumerator<char> enumerator = buffer.Split(null); // null is treated as empty string
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            enumerator = buffer.Split("");
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            enumerator = buffer.Split(" ");
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            // Empty buffer
+            buffer = ReadOnlySpan<char>.Empty;
+
+            enumerator = buffer.Split(null);
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            enumerator = buffer.Split("");
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            enumerator = buffer.Split(" ");
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.False(enumerator.MoveNext());
+
+            // Single whitespace buffer
+            buffer = " ".AsSpan();
+
+            enumerator = buffer.Split(null); // null is treated as empty string
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(1, 1));
+            Assert.False(enumerator.MoveNext());
+
+            enumerator = buffer.Split("");
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(1, 1));
+            Assert.False(enumerator.MoveNext());
+
+            enumerator = buffer.Split(" ");
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(0, 0));
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(enumerator.Current, new Range(1, 1));
+            Assert.False(enumerator.MoveNext());
         }
 
         [Theory]
-        [InlineData("", 'a')]
-        [InlineData("a", 'a')]
-        [InlineData("aaaa", 'a')]
-        [InlineData("abababab", 'a')]
-        [InlineData("babababa", 'a')]
-        [InlineData("aaaaab", 'a')]
-        [InlineData("baaaaa", 'a')]
-        [InlineData("zzzzzazzzzazzzzazzz", 'z')]
-        public void SplitOneCharRemoveEmpty(string str, char separator)
+        [InlineData("", ',', new[] { "" })]
+        [InlineData(" ", ' ', new[] { "", "" })]
+        [InlineData(",", ',', new[] { "", "" })]
+        [InlineData("     ", ' ', new[] { "", "", "", "", "", "" })]
+        [InlineData(",,", ',', new[] { "", "", "" })]
+        [InlineData("ab", ',', new[] { "ab" })]
+        [InlineData("a,b", ',', new[] { "a", "b" })]
+        [InlineData("a,", ',', new[] { "a", "" })]
+        [InlineData(",b", ',', new[] { "", "b" })]
+        [InlineData(",a,b", ',', new[] { "", "a", "b" })]
+        [InlineData("a,b,", ',', new[] { "a", "b", "" })]
+        [InlineData("a,b,c", ',', new[] { "a", "b", "c" })]
+        [InlineData("a,,c", ',', new[] { "a", "", "c" })]
+        [InlineData(",a,b,c", ',', new[] { "", "a", "b", "c" })]
+        [InlineData("a,b,c,", ',', new[] { "a", "b", "c", "" })]
+        [InlineData(",a,b,c,", ',', new[] { "", "a", "b", "c", "" })]
+        [InlineData("first,second", ',', new[] { "first", "second" })]
+        [InlineData("first,", ',', new[] { "first", "" })]
+        [InlineData(",second", ',', new[] { "", "second" })]
+        [InlineData(",first,second", ',', new[] { "", "first", "second" })]
+        [InlineData("first,second,", ',', new[] { "first", "second", "" })]
+        [InlineData("first,second,third", ',', new[] { "first", "second", "third" })]
+        [InlineData("first,,third", ',', new[] { "first", "", "third" })]
+        [InlineData(",first,second,third", ',', new[] { "", "first", "second", "third" })]
+        [InlineData("first,second,third,", ',', new[] { "first", "second", "third", "" })]
+        [InlineData(",first,second,third,", ',', new[] { "", "first", "second", "third", "" })]
+        [InlineData("Foo Bar Baz", ' ', new[] { "Foo", "Bar", "Baz" })]
+        [InlineData("Foo Bar Baz ", ' ', new[] { "Foo", "Bar", "Baz", "" })]
+        [InlineData(" Foo Bar Baz ", ' ', new[] { "", "Foo", "Bar", "Baz", "" })]
+        [InlineData(" Foo  Bar Baz ", ' ', new[] { "", "Foo", "", "Bar", "Baz", "" })]
+        [InlineData("Foo Baz Bar", default(char), new[] { "Foo Baz Bar" })]
+        [InlineData("Foo Baz \x0000 Bar", default(char), new[] { "Foo Baz ", " Bar" })]
+        [InlineData("Foo Baz \x0000 Bar\x0000", default(char), new[] { "Foo Baz ", " Bar", "" })]
+        public static void SpanSplitCharSeparator(string valueParam, char separator, string[] expectedParam)
         {
-            var parts = new List<string>();
+            char[][] expected = expectedParam.Select(x => x.ToCharArray()).ToArray();
+            AssertEqual(expected, valueParam.AsSpan(), valueParam.AsSpan().Split(separator));
+        }
 
-            var span = str.AsSpan();
-            foreach (var range in span.Split(separator))
+        [Theory]
+        [InlineData("", new[] { "" })]
+        [InlineData(" ", new[] { "", "" })]
+        [InlineData("     ", new[] { "", "", "", "", "", "" })]
+        [InlineData("  ", new[] { "", "", "" })]
+        [InlineData("ab", new[] { "ab" })]
+        [InlineData("a b", new[] { "a", "b" })]
+        [InlineData("a ", new[] { "a", "" })]
+        [InlineData(" b", new[] { "", "b" })]
+        [InlineData("Foo Bar Baz", new[] { "Foo", "Bar", "Baz" })]
+        [InlineData("Foo Bar Baz ", new[] { "Foo", "Bar", "Baz", "" })]
+        [InlineData(" Foo Bar Baz ", new[] { "", "Foo", "Bar", "Baz", "" })]
+        [InlineData(" Foo  Bar Baz ", new[] { "", "Foo", "", "Bar", "Baz", "" })]
+        public static void SpanSplitDefaultCharSeparator(string valueParam, string[] expectedParam)
+        {
+            char[][] expected = expectedParam.Select(x => x.ToCharArray()).ToArray();
+            AssertEqual(expected, valueParam.AsSpan(), valueParam.AsSpan().Split());
+        }
+
+        [Theory]
+        [InlineData(" Foo Bar Baz,", ", ", new[] { " Foo Bar Baz," })]
+        [InlineData(" Foo Bar Baz, ", ", ", new[] { " Foo Bar Baz", "" })]
+        [InlineData(", Foo Bar Baz, ", ", ", new[] { "", "Foo Bar Baz", "" })]
+        [InlineData(", Foo, Bar, Baz, ", ", ", new[] { "", "Foo", "Bar", "Baz", "" })]
+        [InlineData(", , Foo Bar, Baz", ", ", new[] { "", "", "Foo Bar", "Baz" })]
+        [InlineData(", , Foo Bar, Baz, , ", ", ", new[] { "", "", "Foo Bar", "Baz", "", "" })]
+        [InlineData(", , , , , ", ", ", new[] { "", "", "", "", "", "" })]
+        [InlineData("     ", " ", new[] { "", "", "", "", "", "" })]
+        [InlineData("  Foo, Bar  Baz  ", "  ", new[] { "", "Foo, Bar", "Baz", "" })]
+        public static void SpanSplitStringSeparator(string valueParam, string separator, string[] expectedParam)
+        {
+            char[][] expected = expectedParam.Select(x => x.ToCharArray()).ToArray();
+            AssertEqual(expected, valueParam.AsSpan(), valueParam.AsSpan().Split(separator));
+        }
+
+        private static void AssertEqual<T>(T[][] items, ReadOnlySpan<T> orig, SpanSplitEnumerator<T> source) where T : IEquatable<T>
+        {
+            foreach (var item in items)
             {
-                var part = span[range];
-                if (!part.IsEmpty)
-                    parts.Add(part.ToString());
+                Assert.True(source.MoveNext());
+                var slice = orig[source.Current];
+                Assert.Equal(item, slice.ToArray());
             }
-
-            Assert.Equal(str.Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries), parts);
-        }
-
-        [Theory]
-        [InlineData("aaababbaaabbabbaba", "bb")]
-        [InlineData("aaaa\r\nbbb\r\ncccc\ncc\r\ndddd", "\r\n")]
-        [InlineData("aaababbaaabbabbaba", "")]
-        public void SplitAllChars(string str, string separator)
-        {
-            var parts = new List<string>();
-
-            var span = str.AsSpan();
-            foreach (var range in span.Split(separator))
-                parts.Add(span[range].ToString());
-
-            Assert.Equal(str.Split(new[] { separator }, StringSplitOptions.None), parts);
+            Assert.False(source.MoveNext());
         }
     }
 }
--- MiscUtil/SpanSplitExtensions.cs
+++ MiscUtil/SpanSplitExtensions.cs
@@ -1,63 +1,119 @@
 ﻿using System;
 
-// Borrowed from here until it's merged into dotnet
-// https://github.com/dotnet/runtime/blob/2da605832d09f8bd046517b463652cc52ec6553e/src/libraries/System.Private.CoreLib/src/System/MemoryExtensions.Split.cs
+// Borrowed from here until .net 5
+// https://github.com/dotnet/runtime/blob/master/src/libraries/System.Private.CoreLib/src/System/SpanSplitEnumerator.T.cs
 
 namespace MiscUtil
 {
     public static class SpanSplitExtensions
     {
+        /// <summary>
+        /// Returns a type that allows for enumeration of each element within a split span
+        /// using a single space as a separator character.
+        /// </summary>
+        /// <param name="span">The source span to be enumerated.</param>
+        /// <returns>Returns a <see cref="System.SpanSplitEnumerator{T}"/>.</returns>
+        public static SpanSplitEnumerator<char> Split(this ReadOnlySpan<char> span)
+            => new SpanSplitEnumerator<char>(span, ' ');
+
+        /// <summary>
+        /// Returns a type that allows for enumeration of each element within a split span
+        /// using the provided separator character.
+        /// </summary>
+        /// <param name="span">The source span to be enumerated.</param>
+        /// <param name="separator">The separator character to be used to split the provided span.</param>
+        /// <returns>Returns a <see cref="System.SpanSplitEnumerator{T}"/>.</returns>
         public static SpanSplitEnumerator<char> Split(this ReadOnlySpan<char> span, char separator)
             => new SpanSplitEnumerator<char>(span, separator);
 
+        /// <summary>
+        /// Returns a type that allows for enumeration of each element within a split span
+        /// using the provided separator string.
+        /// </summary>
+        /// <param name="span">The source span to be enumerated.</param>
+        /// <param name="separator">The separator string to be used to split the provided span.</param>
+        /// <returns>Returns a <see cref="System.SpanSplitEnumerator{T}"/>.</returns>
         public static SpanSplitEnumerator<char> Split(this ReadOnlySpan<char> span, string separator)
-            => new SpanSplitEnumerator<char>(span, separator.AsSpan());
+            => new SpanSplitEnumerator<char>(span, (separator ?? string.Empty).AsSpan());
     }
 
+    /// <summary>
+    /// <see cref="System.SpanSplitEnumerator{T}"/> allows for enumeration of each element within a <see cref="System.ReadOnlySpan{T}"/>
+    /// that has been split using a provided separator.
+    /// </summary>
     public ref struct SpanSplitEnumerator<T> where T : IEquatable<T>
     {
-        private readonly ReadOnlySpan<T> _sequence;
-        private readonly T _separator;
-        private readonly ReadOnlySpan<T> _separators;
-        private readonly int _increment;
-        private int _offset;
-        private int _index;
+        private readonly ReadOnlySpan<T> _buffer;
 
+        private readonly ReadOnlySpan<T> _separators;
+        private readonly T _separator;
+
+        private readonly int _separatorLength;
+        private readonly bool _splitOnSingleToken;
+
+        private readonly bool _isInitialized;
+
+        private int _startCurrent;
+        private int _endCurrent;
+        private int _startNext;
+
+        /// <summary>
+        /// Returns an enumerator that allows for iteration over the split span.
+        /// </summary>
+        /// <returns>Returns a <see cref="System.SpanSplitEnumerator{T}"/> that can be used to iterate over the split span.</returns>
         public SpanSplitEnumerator<T> GetEnumerator() => this;
 
-        internal SpanSplitEnumerator(ReadOnlySpan<T> span, T separator)
-        {
-            _sequence = span;
-            _separator = separator;
-            _separators = ReadOnlySpan<T>.Empty;
-            _increment = 0;
-            _index = 0;
-            _offset = 0;
-        }
+        /// <summary>
+        /// Returns the current element of the enumeration.
+        /// </summary>
+        /// <returns>Returns a <see cref="System.Range"/> instance that indicates the bounds of the current element withing the source span.</returns>
+        public Range Current => new Range(_startCurrent, _endCurrent);
 
         internal SpanSplitEnumerator(ReadOnlySpan<T> span, ReadOnlySpan<T> separators)
         {
-            _sequence = span;
-            _separator = default!;
+            _isInitialized = true;
+            _buffer = span;
             _separators = separators;
-            _increment = Math.Max(0, separators.Length - 1);
-            _index = 0;
-            _offset = 0;
+            _separator = default!;
+            _splitOnSingleToken = false;
+            _separatorLength = _separators.Length != 0 ? _separators.Length : 1;
+            _startCurrent = 0;
+            _endCurrent = 0;
+            _startNext = 0;
         }
 
-        public Range Current => new Range(_offset, _offset + _index - 1);
+        internal SpanSplitEnumerator(ReadOnlySpan<T> span, T separator)
+        {
+            _isInitialized = true;
+            _buffer = span;
+            _separator = separator;
+            _separators = default;
+            _splitOnSingleToken = true;
+            _separatorLength = 1;
+            _startCurrent = 0;
+            _endCurrent = 0;
+            _startNext = 0;
+        }
 
+        /// <summary>
+        /// Advances the enumerator to the next element of the enumeration.
+        /// </summary>
+        /// <returns><see langword="true"/> if the enumerator was successfully advanced to the next element; <see langword="false"/> if the enumerator has passed the end of the enumeration.</returns>
         public bool MoveNext()
         {
-            if (_sequence.Length - _offset < _index) { return false; }
-            var slice = _sequence.Slice(_offset += _index + (_index == 0 ? 0 : _increment));
+            if (!_isInitialized || _startNext > _buffer.Length)
+            {
+                return false;
+            }
 
-            var nextIdx = _separators.IsEmpty
-                ? slice.IndexOf(_separator)
-                : slice.IndexOf(_separators);
-            _index = (nextIdx != -1 ? nextIdx : slice.Length) + 1;
-            if (_offset + _index > _sequence.Length + 1)
-                _index = _sequence.Length + 1 - _offset;
+            ReadOnlySpan<T> slice = _buffer.Slice(_startNext);
+            _startCurrent = _startNext;
+
+            int separatorIndex = _splitOnSingleToken ? slice.IndexOf(_separator) : slice.IndexOf(_separators);
+            int elementLength = separatorIndex != -1 ? separatorIndex : slice.Length;
+
+            _endCurrent = _startCurrent + elementLength;
+            _startNext = _endCurrent + _separatorLength;
             return true;
         }
     }
