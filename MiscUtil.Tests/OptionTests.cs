6e6397deeaa90938ae2150877ab717360f3c5eb7
Joel Mueller <joel.mueller@spglobal.com>
2022-12-06 09:33:51 -0600

    tests

--- MiscUtil.Tests/OptionTests.cs
+++ MiscUtil.Tests/OptionTests.cs
@@ -117,12 +117,14 @@ public class OptionTests
         var timespan = Option.Parse<TimeSpan>("05:11:04");
         var fraction = Option.Parse<double>("3.14");
         var guid = Option.Parse<Guid>("ac439fd6-9b64-42f3-bc74-38017c97b965");
+        var nothing = Option.Parse<int>("foo");
 
         Assert.True(integer.IsSome(out var i) && i == 12345);
         Assert.True(date.IsSome(out var d) && d == new DateTime(2023, 06, 17));
         Assert.True(timespan.IsSome(out var t) && t == new TimeSpan(5, 11, 4));
         Assert.True(fraction.IsSome(out var x) && x == 3.14);
         Assert.True(guid.IsSome(out var g) && g == new Guid("ac439fd6-9b64-42f3-bc74-38017c97b965"));
+        Assert.True(nothing.IsNone);
     }
 
     [Fact]
@@ -133,12 +135,14 @@ public class OptionTests
         var timespan = Option.Parse<TimeSpan>("05:11:04".AsSpan());
         var fraction = Option.Parse<double>("3.14".AsSpan());
         var guid = Option.Parse<Guid>("ac439fd6-9b64-42f3-bc74-38017c97b965".AsSpan());
+        var nothing = Option.Parse<int>("foo".AsSpan());
 
         Assert.True(integer.IsSome(out var i) && i == 12345);
         Assert.True(date.IsSome(out var d) && d == new DateTime(2023, 06, 17));
         Assert.True(timespan.IsSome(out var t) && t == new TimeSpan(5, 11, 4));
         Assert.True(fraction.IsSome(out var x) && x == 3.14);
         Assert.True(guid.IsSome(out var g) && g == new Guid("ac439fd6-9b64-42f3-bc74-38017c97b965"));
+        Assert.True(nothing.IsNone);
     }
 
 #endif
