dcfe994e65f3144f7a49ea7c3dcb5ab5fb169c16
Joel Mueller <joel.mueller@gmail.com>
2022-12-06 23:36:01 -0600

    Better IComparable tests

--- MiscUtil.Tests/OptionTests.cs
+++ MiscUtil.Tests/OptionTests.cs
@@ -403,17 +403,19 @@ public class OptionTests
         var b = Option.Some(2);
         var c = Option.Some(3);
         var d = Option.Some(4);
+        var n = Option<int>.None;
 
 #pragma warning disable CS1718 // Comparison made to same variable
         Assert.True(b > a);
         Assert.True(b >= b);
         Assert.True(c < d);
         Assert.True(c <= c);
+        Assert.True(n < a);
 #pragma warning restore CS1718 // Comparison made to same variable
 
-        var items = new[] { d, b, c, a };
+        var items = new[] { d, b, n, c, a };
         Array.Sort(items);
-        Assert.Equal(new[] { a, b, c, d }, items);
+        Assert.Equal(new[] { n, a, b, c, d }, items);
     }
 
 #if NET7_0_OR_GREATER
